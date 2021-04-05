a7080b46aefa89702daa1e9693a7997f99079ccd
Shhh <shanwilkinson2@users.noreply.github.com>
2021-04-05 19:27:00 +0100

    mvp app

--- get scootloop location data.R
+++ get scootloop location data.R
@@ -39,69 +39,47 @@ scoot_loc2 <- scoot_loc %>%
   filter(!is.na(start_location_point)) %>%
   # scn seems to link junctions, but just with a different last letter. 
   # so remove last letter
-  mutate(junction = str_sub(scn, 1, -2)) %>%
+  mutate(junction = stringr::str_sub(scn, 1, -2)) %>%
   # well known text ie common text format for points
   # works with col num but not name. needs missings deleted & unnesting
   # col 8 = end location. All scoots point towards the junction, so end is at the junction
   st_as_sf(wkt = 8, crs = 4326) # lat/ long
 
-# # all in one go - not working
-# 
-# alltogether <- httr::GET(
-#   url = "https://api.tfgm.com/odata/ScootLoops?$expand=StartLocation,EndLocation&$top=10",
-#   config = headers
-#   ) %>%
-# # parse as text
-#   content("text") %>%
-#   fromJSON()
-# 
-#   
-#   fromJSON(content(response, "text"))$value %>%
-#   janitor::clean_names()
-# # start & end points are each in thier little spatial data frame
-# # not got these to read as spatial yet tho
-# scoot_loc2 <- scoot_loc %>% 
-#   mutate(start_location_point = start_location$LocationSpatial$Geography$WellKnownText,
-#          end_location_point = end_location$LocationSpatial$Geography$WellKnownText) %>%
-#   select(-c(start_location, end_location)) %>%
-#   # make date format
-#   mutate(last_updated = as.POSIXct(last_updated)) %>%
-#   # missing locations don't seem to be being updated anyway
-#   filter(!is.na(start_location_point)) %>%
-#   # scn seems to link junctions, but just with a different last letter. 
-#   # so remove last letter
-#   mutate(junction = str_sub(scn, 1, -2)) %>%
-#   # well known text ie common text format for points
-#   # works with col num but not name. needs missings deleted & unnesting
-#   # col 8 = end location. All scoots point towards the junction, so end is at the junction
-#   st_as_sf(wkt = 8, crs = 4326) # lat/ long
-
-
-
 # chorley new scoots
-chorley_new <- c(2338, 2335, 2334, 2336) #2337 seems to have been discontinued
+    chorley_new <- c(2338, 2335, 2334, 2336) #2337 seems to have been discontinued
+    
+    chorley_new_scoots_end <- scoot_loc2 %>%
+      filter(id %in% chorley_new)
+    
+    chorley_new_scoots_start <- chorley_new_scoots_end %>%
+      st_drop_geometry() %>%
+      # well known text ie common text format for points
+      # works with col num but not name. needs missings deleted & unnesting
+      st_as_sf(wkt = 7, crs = 4326) # lat/ long
 
-chorley_new_scoots_end <- scoot_loc2 %>%
-  filter(id %in% chorley_new)
-
-chorley_new_scoots_start <- chorley_new_scoots_end %>%
-  st_drop_geometry() %>%
-  # well known text ie common text format for points
-  # works with col num but not name. needs missings deleted & unnesting
-  st_as_sf(wkt = 7, crs = 4326) # lat/ long
-
+# get list of unique junctions (from SCN minus last character)  
+    all_junctions <- scoot_loc2 %>% 
+      # drop geometry
+      st_drop_geometry() %>%
+      # filter out any that may not be being currently updated 
+      # ie at least in last fortnight
+      filter(last_updated >= max(last_updated - lubridate::weeks(2))) %>%
+      select(junction) %>%
+      unique()
+    
 
 # how many scoots does each juntion have?
-scoot_loc2 %>%
-  st_drop_geometry() %>%
-  # only scoots that have been updated in the last 2 weeks
-  filter(last_updated >= max(last_updated - lubridate::weeks(2))) %>%
-  group_by(junction) %>%
-  summarise(num_scoots = n()) %>%
-  ungroup() %>%
-  summarise(   min_scoots = min(num_scoots), 
-            max_scoots = max(num_scoots),
-            median_scoots = median(num_scoots),
-            cenile_25_scoots = quantile(num_scoots, 0.25),
-            cenile_75_scoots = quantile(num_scoots, 0.75)
-            )
+  scoot_loc2 %>%
+    st_drop_geometry() %>%
+    # only scoots that have been updated in the last 2 weeks
+    filter(last_updated >= max(last_updated - lubridate::weeks(2))) %>%
+    group_by(junction) %>%
+    summarise(num_scoots = n()) %>%
+    ungroup() %>%
+    summarise(   min_scoots = min(num_scoots), 
+              max_scoots = max(num_scoots),
+              median_scoots = median(num_scoots),
+              mean_scoots = mean(num_scoots),
+              cenile_25_scoots = quantile(num_scoots, 0.25),
+              cenile_75_scoots = quantile(num_scoots, 0.75)
+              )
--- scootloop_app/app.R
+++ scootloop_app/app.R
@@ -16,31 +16,52 @@ library(lubridate)
 library(leaflet)
 library(leaflet.extras)
 
-# get all scootloop locations
-    response <- httr::GET(
-        url = "https://api.tfgm.com/odata/ScootLoops?$expand=StartLocation,EndLocation",
-        config = headers)
-    
-    # parse as text
-    scoot_loc2 <- fromJSON(content(response, "text"))$value %>%
-        janitor::clean_names() %>%
-     # start & end points are each in thier little spatial data frame
-        mutate(start_location_point = start_location$LocationSpatial$Geography$WellKnownText,
-               end_location_point = end_location$LocationSpatial$Geography$WellKnownText) %>%
-        select(-c(start_location, end_location)) %>%
-        # make date format
-        mutate(last_updated = as.POSIXct(last_updated)) %>%
-        # missing locations don't seem to be being updated anyway
-        filter(!is.na(start_location_point)) %>%
-        # scn seems to link junctions, but just with a different last letter. 
-        # so remove last letter
-        mutate(junction = stringr::str_sub(scn, 1, -2)) %>%
-        # well known text ie common text format for points
-        # works with col num but not name. needs missings deleted & unnesting
-        # col 8 = end location. All scoots point towards the junction, so end is at the junction
-        st_as_sf(wkt = 8, crs = 4326) # lat/ long
+# non responsive stuff ##########################
 
-# Define UI for application that draws a histogram
+    source("api_key.R")
+
+    # api login details in headers
+    # login
+    headers <- add_headers("Ocp-Apim-Subscription-Key" = mykey)
+    
+    # get all scootloop locations
+        response <- httr::GET(
+            url = "https://api.tfgm.com/odata/ScootLoops?$expand=StartLocation,EndLocation",
+            config = headers)
+            
+        # parse as text
+        scoot_loc2 <- fromJSON(content(response, "text"))$value %>%
+            janitor::clean_names() %>%
+         # start & end points are each in thier little spatial data frame
+            mutate(start_location_point = start_location$LocationSpatial$Geography$WellKnownText,
+                   end_location_point = end_location$LocationSpatial$Geography$WellKnownText) %>%
+            select(-c(start_location, end_location)) %>%
+            # make date format
+            mutate(last_updated = as.POSIXct(last_updated)) %>%
+            # missing locations don't seem to be being updated anyway
+            filter(!is.na(start_location_point)) %>%
+            # scn seems to link junctions, but just with a different last letter. 
+            # so remove last letter
+            mutate(junction = stringr::str_sub(scn, 1, -2)) %>%
+            # well known text ie common text format for points
+            # works with col num but not name. needs missings deleted & unnesting
+            # col 8 = end location. All scoots point towards the junction, so end is at the junction
+            st_as_sf(wkt = 8, crs = 4326) # lat/ long
+    
+        # get list of unique junctions (from SCN minus last character)  
+        all_junctions <- scoot_loc2 %>% 
+            # drop geometry
+            st_drop_geometry() %>%
+            # filter out any that may not be being currently updated 
+            # ie at least in last fortnight
+            filter(last_updated >= max(last_updated - lubridate::weeks(2))) %>%
+            select(junction) %>%
+            arrange(junction) %>%
+            unique()    
+ 
+###############################
+           
+# Define UI for app
 ui <- fluidPage(
 
     # Application title
@@ -49,11 +70,16 @@ ui <- fluidPage(
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
-            # sliderInput("bins",
-            #             "Number of bins:",
-            #             min = 1,
-            #             max = 50,
-            #             value = 30)
+            # dropdown. selectize = TRUE lets you type in
+            selectInput(
+                inputId = "select_junction",
+                label = "Select junction:",
+                choices = all_junctions,
+                selected = "N53211",
+                selectize = TRUE
+            ),
+            em("(Delete selected junction to type & search)"),
+            p("Junction numbers on map")
         ),
 
         # Show a plot of the generated distribution
@@ -66,16 +92,32 @@ ui <- fluidPage(
                         tabPanel("All scoots map",
                                  leafletOutput("scoot_location_plot")
                                  ),
+                        tabPanel("Selected scoots",
+                            DT::DTOutput("selected_jct_table"),
+                            h3("Explanation of fields"),
+                            p("Id, SCN = id numbers for the scootloop"),
+                            p("Description = SCN or a written description of the location"),
+                            p("Last updated = date/ time of update. **Time always in GMT - need to sort this.**"),
+                            p("Congestion percentage = not totally sure"),
+                            p("Current flow = vehicles in 5 mins (I think)"),
+                            p("Average speed = average speed in 5 mins (I think). I think the scoots measure in KMPH so converted to MPH as well."),
+                            p("Link status = not totally sure"),
+                            p("Link travel time = in seconds? Not totally sure.")
+                        ),
                         tabPanel("About",
                                  p("This data contains details of SCOOT loops on highways in Greater Manchester."),
                                  p("SCOOT is a measure of congestion at traffic signals."),
-                                 p("Contains Transport for Greater Manchester data. Contains OS data © Crown copyright and database right 2016."))
+                                 p("Contains Transport for Greater Manchester data. Contains OS data © Crown copyright and database right 2016."),
+                                 a("TfGM", href = "https://developer.tfgm.com/developer", target = "_blank"),
+                                 p("Code for this app is on my github."),
+                                 a("Github", href = "https://github.com/shanwilkinson2/tfgm_scoot_loops", target = "_blank")
+                                 )
             )
         )
     )
 )
 
-# Define server logic required to draw a histogram
+# Define server logic 
 server <- function(input, output) {
     
     # map of scootloop locations   
@@ -84,20 +126,41 @@ server <- function(input, output) {
             addProviderTiles("Stamen.TonerLite") %>%
             addResetMapButton %>%
             addCircleMarkers(radius = 3, color = "blue", 
-                             popup = ~glue("Last updated: {format(last_updated, '%d/%m/%y')}<br>ID: {id}<br>SCN: {scn}<br>Desription: {description} <br> (end of scoot)")
+                             popup = ~glue::glue("Last updated: {format(last_updated, '%d/%m/%y')}<br>Scoot id: {id}<br>Junction number: {junction}<br>Desription: {description} <br> (end of scoot)")
             ) %>%
-            addControl(glue("<b>Location of GM scootloops</b><br>Click on points for more detail"), 
+            addControl(glue::glue("<b>Location of GM scootloops</b><br>Click on points for more detail"), 
                        position = "topright")
     })
     
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    # reactive dataset for seleted junction
+    selected_jct_data <- reactive({
+        # endpoint for selected junction
+        endpoint <- glue::glue("https://api.tfgm.com/odata/ScootLoops?$expand=ScootDetails&$filter=startswith(SCN,'{input$select_junction}')")
+        # pull data
+        response <- httr::GET(
+            url = endpoint,
+            config = headers)
+        # process data
+        # parse as text
+        scoot_dat <- fromJSON(content(response, "text"))$value %>%
+            select(-c(Id, SCN, StartLocationId, EndLocationId)) %>%
+            tidyr::unnest(cols = ScootDetails) %>%
+            rename(average_speed_kmph = AverageSpeed) %>%
+            mutate(average_speed_mph = round(average_speed_kmph * 0.62137119223733, 0))
     })
+    
+    # generate table for selected junction
+    output$selected_jct_table <- DT::renderDT({
+        data = selected_jct_data() %>%
+            select(Id, SCN, Description, 
+                   "Last updated" = LastUpdated, "Congestion percentage" = CongestionPercentage,
+                   "Current flow" = CurrentFlow, "Average speed (kmph)" = average_speed_kmph, 
+                   "Average speed (mph)" = average_speed_mph, "Link status" = LinkStatus,
+                   "Link travel time" = LinkTravelTime
+                   )
+        },
+        rownames = FALSE)
+    
 }
 
 # Run the application 
